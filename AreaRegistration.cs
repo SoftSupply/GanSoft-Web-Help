edda02c6df992e6396f29add9fbafa16e98d1090
SoftSupply <desarrollo@softsupply.com>
2019-06-26 12:33:27 -0400

    Generic Template
    Init1

---
+++ AreaRegistration.cs
@@ -0,0 +1,24 @@
+﻿using System.Web.Mvc;
+
+namespace SoftSupply.Help.Areas.Reseller
+{
+    public class ResellerAreaRegistration : AreaRegistration
+    {
+        public override string AreaName
+        {
+            get
+            {
+                return "Reseller";
+            }
+        }
+
+        public override void RegisterArea(AreaRegistrationContext context)
+        {
+            context.MapRoute(
+                AreaName,
+                AreaName + "/{controller}/{action}/{id}",
+                new { controller = "Default", action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
